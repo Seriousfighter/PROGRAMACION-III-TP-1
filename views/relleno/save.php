4af40c3eb86985bd3c2119dc4c2c763cdfd6321b
Alan Schneider <schneideralan130892@gmail.com>
2026-06-09 15:11:01 -0300

    Fix form action URL for edit and create modes

--- views/relleno/save.php
+++ views/relleno/save.php
@@ -5,7 +5,10 @@
 </head>
 <body>
     <h1><?= isset($relleno) ? 'Editar' : 'Nuevo' ?> Relleno</h1>
-    <form action="<?= isset($relleno) ? '/rellenos/' . $relleno->id . '/update' : '/rellenos/store' ?>" method="POST">
+    <form action="<?= isset($relleno) ? '/rellenos/' . $relleno->id : '/rellenos' ?>" method="POST">
+        <?php if (isset($relleno)): ?>
+            <input type="hidden" name="_method" value="PUT">
+        <?php endif; ?>
         <label>Nombre: <input type="text" name="nombre" value="<?= isset($relleno) ? htmlspecialchars($relleno->nombre) : '' ?>" required></label><br>
         <label>Precio Extra: <input type="number" step="0.01" name="precio_extra" value="<?= isset($relleno) ? htmlspecialchars((string)$relleno->precio_extra) : '0' ?>"></label><br>
         <button type="submit">Guardar</button>
