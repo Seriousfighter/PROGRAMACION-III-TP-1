b300b225abfbf8418ad04d639f5629336ffa40b1
alan <schneideralan130892@gmail.com>
2026-06-15 22:19:27 -0300

    fixed nav

--- views/UI/nav.php
+++ views/UI/nav.php
@@ -3,13 +3,8 @@
     <a href="/ingredients">Ingredientes</a>
     <a href="/coberturas">Coberturas</a>
     <a href="/rellenos">Rellenos</a>
-    <a href="/register">Registro</a>
-    <?= isLogged() ? 
-        '<a href="/panel">Panel</a>
-        <form method="POST" action="/logout" style="display:inline;">
-            <button type="submit">Logout</button>
-        </form>' : 
-        '<a href="/login">Login</a>
-        <a href="/register">Register</a>' 
-    ?>
+    
+    <?php if (!isLogged()): ?>
+        <a href="/register">Register</a>
+    <?php endif; ?>
 </nav>
